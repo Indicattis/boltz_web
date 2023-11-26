a77461f7f4f8f13196f151057171acbcd5c2f4ce
Indicattis <joao.staehler@gmail.com>
2023-11-26 15:08:16 -0300

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,5 @@
 // LOCALHOST
-export const serverUrl = '//127.0.0.1:9001'
+// export const serverUrl = '//127.0.0.1:9001'
+
+// AWS
+export const serverUrl = 'https://thelordbarber-server.devliners.com.br'
